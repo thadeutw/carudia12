369297ffbd8f3a82ebb5d85ce1e772d6ed1165d4
thadeutw <49984240+thadeutw@users.noreply.github.com>
2021-01-21 16:39:05 -0300

    Add files via upload

---
+++ Dark-main/src/darkmenu.js
@@ -0,0 +1,40 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Dark:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *DARK*
+  DUVIDAS? 👇
+  WA.me/5517991134416
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
